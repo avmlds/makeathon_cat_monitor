714d3fe8d4c91da0ae06b488ecd0232bc34cbcf5
alexander <alexander.lopatnikov@lumiprobe.com>
2021-04-24 22:36:00 +0300

    Bot dont send spam anymore

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS telegram_updates (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     status VARCHAR NOT NULL,
     update_id INTEGER NOT NULL,
-    message_id INTEGER NOT NULL UNIQUE,
+    message_id INTEGER NOT NULL,
     message_from_id INTEGER NOT NULL, 
     message_from_is_bot BOOL NOT NULL, 
     message_from_first_name VARCHAR NOT NULL, 
@@ -36,6 +36,7 @@ CREATE TABLE IF NOT EXISTS user (
     telegram_id VARCHAR UNIQUE NOT NULL, 
     username VARCHAR NOT NULL, 
     first_name VARCHAR NOT NULL, 
+    system_timestamp TEXT NOT NULL,
     date TEXT NOT NULL,
     live_period INTEGER NOT NULL,
     is_active BOOL NOT NULL DEFAULT FALSE);''')
@@ -46,6 +47,7 @@ CREATE TABLE IF NOT EXISTS user_tracking (
     message_id INTEGER NOT NULL,
     latitude FLOAT NOT NULL,
     longitude FLOAT NOT NULL,
+    system_timestamp TEXT NOT NULL,
     date TEXT NOT NULL,
     live_period INTEGER NOT NULL,
     heading FLOAT, 
@@ -55,6 +57,8 @@ CREATE TABLE IF NOT EXISTS pet_tracking (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     user_id INTEGER REFERENCES user(id) ON UPDATE CASCADE,
     tracking_id INTEGER REFERENCES user_tracking(id) ON UPDATE CASCADE,
+    message_id INTEGER REFERENCES user_tracking(message_id) ON UPDATE CASCADE,
+    system_timestamp TEXT NOT NULL,
     pet_type VARCHAR NOT NULL,
     sex VARCHAR NOT NULL,
     color VARCHAR NOT NULL,
@@ -64,6 +68,75 @@ CREATE TABLE IF NOT EXISTS pet_tracking (
     is_ill BOOL NOT NULL,
     in_danger BOOL NOT NULL);'''
                )
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS proceeded_messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    update_id INTEGER UNIQUE NOT NULL,
+    is_resolved BOOL NOT NULL DEFAULT TRUE
+);'''
+               )
+
 connection.commit()
 connection.close()
 ```
+
+
+            """
+            if not response['ok']:
+                return {'Messages': 'No new'}
+
+            for message_response in parsed_response.result:
+                if 'message' in message_response.dict().keys():
+                    data = {'status': parsed_response.ok,
+                            'update_id': message_response.update_id,
+                            'message_id': message_response.message.message_id,
+                            'message_from_id': message_response.message.from_.id,
+                            'message_from_is_bot': message_response.message.from_.is_bot,
+                            'message_from_first_name': message_response.message.from_.first_name,
+                            'message_from_username': message_response.message.from_.username,
+                            'message_from_language_code': message_response.message.from_.language_code,
+                            'message_chat_id': message_response.message.chat.id,
+                            'message_chat_first_name': message_response.message.chat.first_name,
+                            'message_chat_username': message_response.message.chat.username,
+                            'message_chat_type': message_response.message.chat.type,
+                            'date': message_response.message.date.isoformat()}
+
+                    if message_response.message.entities is not None:
+                        data.update({'entities_offset': message_response.message.entities[0].offset,
+                                     'entities_length': message_response.message.entities[0].length,
+                                     'entities_type': message_response.message.entities[0].type})
+                    else:
+                        data.update({'entities_offset': None, 'entities_length': None, 'entities_type': None})
+
+                    if message_response.message.location is not None:
+                        data.update({'longitude': message_response.message.location.longitude,
+                                     'latitude': message_response.message.location.latitude,
+                                     'live_period': message_response.message.location.live_period,
+                                     })
+                    else:
+                        data.update({'longitude': None, 'latitude': None})
+
+                    if message_response.message.text is not None:
+                        data.update({'text': message_response.message.text})
+                    else:
+                        data.update({'text': None})
+
+                async with aiosqlite.connect('../db/animal_monitor.db') as db:
+                    await db.execute(INSERT_UPDATES % data)
+                    await db.commit()
+
+            if response['ok']:
+                for result in response['result']:
+                    async with aiosqlite.connect('../db/animal_monitor.db') as db:
+                        res = await db.execute(CHECK_IS_ANSWERED.format(message_id=result['message']['message_id'],
+                                                                        chat_id=result['message']['chat']['id']))
+                        row = await res.fetchall()
+                        chat_id = row[0][0]
+                        message_id = row[0][1]
+                        is_answered = row[0][2]
+                        if not is_answered:
+                            await self.message_handler(result)
+
+                        await db.execute(UPDATE_ANSWERED.format(message_id=message_id, chat_id=chat_id))
+                        await db.commit()
+            """
--- lib/telegram.py
+++ lib/telegram.py
@@ -6,6 +6,7 @@ from lib.models import TelegramUpdateResponse, TelegramMessageResult
 from lib.sql_queries import INSERT_UPDATES, UPDATE_ANSWERED, CHECK_IS_ANSWERED
 from messages.message_texts import START_MESSAGE_TEXT
 from datetime import datetime
+from warnings import warn
 
 
 class TelegramBot:
@@ -26,101 +27,58 @@ class TelegramBot:
 
     async def update_polls(self):
         while True:
-            await asyncio.sleep(3)
+            await asyncio.sleep(5)
             response = await TelegramBot.async_request(session_kwargs={},
                                                        request_kwargs={'method': 'POST',
                                                                        'url': self.update_polls_command})
             parsed_response = TelegramUpdateResponse.parse_obj(response)
             if not parsed_response.ok:
                 raise Exception('Status is not OK')
-            for data in parsed_response.result:
-                if data.message is not None:
-                    telegram_id = data.message.from_.id
-                    chat_id = data.message.from_.id
 
+            for data in parsed_response.result:
+                if await TelegramBot.is_update_resolved(data.update_id):
+                    continue
+                await TelegramBot.track_update(update_id=data.update_id)
+
+                if data.message is not None:
+                    update_id = data.update_id
+                    telegram_id = data.message.from_.id
+                    message_id = data.message.message_id
+                    chat_id = data.message.chat.id
                     if data.message.location is not None and data.message.location.live_period is not None:
                         # start_tracking
                         await self.start_tracking_user(data=data)
 
+                    elif data.message.location is not None and data.message.location.live_period is None:
+                        await self.send_message(chat_id=chat_id, text='Вы должны отправить отслеживание ' \
+                                                                      'геопозиции, а не одиночную геопозицию')
+
                 else:
                     telegram_id = data.edited_message.from_.id
-                    chat_id = data.edited_message.from_.id
+                    update_id = data.update_id
+                    chat_id = data.edited_message.chat.id
+                    message_id = data.edited_message.message_id
+
                     if data.edited_message.location is not None \
                             and data.edited_message.location.live_period is not None \
                             and data.edited_message.location.heading is not None:
-                        await self.continue_tracking_user(data=data)
+                        await TelegramBot.continue_tracking_user(data=data)
 
                 checked_user = await self.check_user_in_db(telegram_id=telegram_id, chat_id=chat_id)
                 if not checked_user[0]:
+                    await TelegramBot.resolve_update(update_id=data.update_id)
                     continue
-
-            if not response['ok']:
-                return {'Messages': 'No new'}
-
-            for message_response in parsed_response.result:
-                if 'message' in message_response.dict().keys():
-                    data = {'status': parsed_response.ok,
-                            'update_id': message_response.update_id,
-                            'message_id': message_response.message.message_id,
-                            'message_from_id': message_response.message.from_.id,
-                            'message_from_is_bot': message_response.message.from_.is_bot,
-                            'message_from_first_name': message_response.message.from_.first_name,
-                            'message_from_username': message_response.message.from_.username,
-                            'message_from_language_code': message_response.message.from_.language_code,
-                            'message_chat_id': message_response.message.chat.id,
-                            'message_chat_first_name': message_response.message.chat.first_name,
-                            'message_chat_username': message_response.message.chat.username,
-                            'message_chat_type': message_response.message.chat.type,
-                            'date': message_response.message.date.isoformat()}
-
-                    if message_response.message.entities is not None:
-                        data.update({'entities_offset': message_response.message.entities[0].offset,
-                                     'entities_length': message_response.message.entities[0].length,
-                                     'entities_type': message_response.message.entities[0].type})
-                    else:
-                        data.update({'entities_offset': None, 'entities_length': None, 'entities_type': None})
-
-                    if message_response.message.location is not None:
-                        data.update({'longitude': message_response.message.location.longitude,
-                                     'latitude': message_response.message.location.latitude,
-                                     'live_period': message_response.message.location.live_period,
-                                     })
-                    else:
-                        data.update({'longitude': None, 'latitude': None})
-
-                    if message_response.message.text is not None:
-                        data.update({'text': message_response.message.text})
-                    else:
-                        data.update({'text': None})
-
-                async with aiosqlite.connect('../db/animal_monitor.db') as db:
-                    await db.execute(INSERT_UPDATES % data)
-                    await db.commit()
-
-            if response['ok']:
-                for result in response['result']:
-                    async with aiosqlite.connect('../db/animal_monitor.db') as db:
-                        res = await db.execute(CHECK_IS_ANSWERED.format(message_id=result['message']['message_id'],
-                                                                        chat_id=result['message']['chat']['id']))
-                        row = await res.fetchall()
-                        chat_id = row[0][0]
-                        message_id = row[0][1]
-                        is_answered = row[0][2]
-                        if not is_answered:
-                            await self.message_handler(result)
-
-                        await db.execute(UPDATE_ANSWERED.format(message_id=message_id, chat_id=chat_id))
-                        await db.commit()
-
-            print(parsed_response)
+                await TelegramBot.resolve_update(update_id=data.update_id)
+                print(data)
 
     async def check_user_in_db(self, telegram_id, chat_id):
         async with aiosqlite.connect('../db/animal_monitor.db') as db:
-            res = await db.execute(f'SELECT id, is_active FROM user WHERE telegram_id = {telegram_id}')
+            res = await db.execute(f'SELECT id, is_active FROM user WHERE telegram_id = {telegram_id};')
             result = await res.fetchall()
             await db.commit()
             if len(result) == 0:
                 await self.send_message(chat_id=chat_id, text=START_MESSAGE_TEXT)
+
                 return False, 0, 0
             elif len(result) == 1:
                 # Bool, id (user), is_active (user)
@@ -130,14 +88,18 @@ class TelegramBot:
 
     async def make_active(self, telegram_id, chat_id):
         async with aiosqlite.connect('../db/animal_monitor.db') as db:
-            res = await db.execute(
-                f'INSERT INTO user VALUES( id, is_active FROM user WHERE telegram_id = {telegram_id}')
+            res = await db.execute(f'INSERT INTO user VALUES( id, is_active FROM user '
+                                   f'WHERE telegram_id = {telegram_id};')
 
-    async def start_tracking_user(self, data):
+    @staticmethod
+    async def start_tracking_user(data):
+        update_id = data.update_id
         user_id = data.message.from_.id
         username = data.message.from_.username
         first_name = data.message.from_.first_name
         message_id = data.message.message_id
+        # system timestamp
+        system_timestamp = datetime.now().isoformat()
         # telegram message timestamp
         iso_timestamp = data.message.date.isoformat()
         live_period = data.message.location.live_period
@@ -146,8 +108,10 @@ class TelegramBot:
         heading = data.message.location.heading
         horizontal_accuracy = data.message.location.horizontal_accuracy
 
-        user_query = f"INSERT OR IGNORE INTO user(telegram_id, username, first_name, date, live_period, is_active) " \
-                     f"VALUES('{user_id}', '{username}', '{first_name}', '{iso_timestamp}', '{live_period}'," \
+        user_query = f"INSERT OR IGNORE INTO user(telegram_id, username, first_name, " \
+                     f"system_timestamp, date, live_period, is_active) " \
+                     f"VALUES('{user_id}', '{username}', '{first_name}', '{system_timestamp}'," \
+                     f"'{iso_timestamp}', '{live_period}'," \
                      f" '{True}');"
         async with aiosqlite.connect('../db/animal_monitor.db') as db:
             query_id_ = await db.execute(user_query)
@@ -155,19 +119,20 @@ class TelegramBot:
             last_row_id = query_id_.lastrowid
         async with aiosqlite.connect('../db/animal_monitor.db') as db:
             tracking_query = f"INSERT INTO user_tracking(user_id, message_id, latitude, longitude, " \
-                             f"date, live_period, heading, horizontal_accuracy) " \
+                             f"system_timestamp, date, live_period, heading, horizontal_accuracy) " \
                              f"VALUES('{last_row_id}', '{message_id}', '{latitude}', '{longitude}', " \
+                             f"'{system_timestamp}'," \
                              f"'{iso_timestamp}', '{live_period}', '{heading}', '{horizontal_accuracy}');"
             inserted_data = await db.execute(tracking_query)
             await db.commit()
 
-            u = 38 / 1
-
-    async def continue_tracking_user(self, data):
+    @staticmethod
+    async def continue_tracking_user(data):
+        update_id = data.update_id
         user_id = data.edited_message.from_.id
-        username = data.edited_message.from_.username
-        first_name = data.edited_message.from_.first_name
         message_id = data.edited_message.message_id
+        # system timestamp
+        system_timestamp = datetime.now().isoformat()
         # telegram message timestamp
         iso_timestamp = data.edited_message.date.isoformat()
         live_period = data.edited_message.location.live_period
@@ -176,39 +141,59 @@ class TelegramBot:
         heading = data.edited_message.location.heading
         horizontal_accuracy = data.edited_message.location.horizontal_accuracy
         async with aiosqlite.connect('../db/animal_monitor.db') as db:
-            query = f'SELECT id FROM user WHERE telegram_id = {user_id}'
+            query = f'SELECT id FROM user WHERE telegram_id = {user_id};'
             user_id_data = await db.execute(query)
             user_id_row = await user_id_data.fetchall()
             await db.commit()
             if len(user_id_row) == 1:
                 user_id = user_id_row[0][0]
-                async with aiosqlite.connect('../db/animal_monitor.db') as db:
-                    tracking_query = f"INSERT INTO user_tracking(user_id, message_id, latitude, longitude, " \
-                                     f"date, live_period, heading, horizontal_accuracy) " \
-                                     f"VALUES('{user_id}', '{message_id}', '{latitude}', '{longitude}', " \
-                                     f"'{iso_timestamp}', '{live_period}', '{heading}', '{horizontal_accuracy}');"
-                    inserted_data = await db.execute(tracking_query)
-                    await db.commit()
+                tracking_query = f"INSERT INTO user_tracking(user_id, message_id, latitude, longitude, " \
+                                 f"system_timestamp, date, live_period, heading, horizontal_accuracy) " \
+                                 f"VALUES('{user_id}', '{message_id}', '{latitude}', '{longitude}', " \
+                                 f"'{system_timestamp}', '{iso_timestamp}', '{live_period}', '{heading}', " \
+                                 f"'{horizontal_accuracy}');"
+                inserted_data = await db.execute(tracking_query)
+                await db.commit()
             else:
-                raise Exception('User is not in DB')
+                warn('User is not in DB')
 
     async def message_handler(self, parsed_response_message):
         result_object = TelegramResult.parse_obj(result)
         if result_object.message.text == '/start':
-            await self.send_message(chat_id=result_object.message.chat.id,
-                                    text=START_MESSAGE_TEXT)
+            await self.send_message(chat_id=result_object.message.chat.id, text=START_MESSAGE_TEXT)
 
     async def send_message(self, chat_id, text):
-        """
-        This func create POST-request and sends it to a Telegram server
-        As a result, user with necessary chat_id will receive message with
-        'text'.
-        :param chat_id: integer
-        :param text: string
-        :return: POST-request to a Telegram server
-        """
         url = self.send_message_command.format(chat_id=chat_id, message_text=text)
         await self.async_request(session_kwargs={}, request_kwargs={'method': 'POST', 'url': url})
+
+    @staticmethod
+    async def resolve_update(update_id):
+        async with aiosqlite.connect('../db/animal_monitor.db') as db:
+            query = f"UPDATE proceeded_messages SET is_resolved = '{True}' " \
+                    f"WHERE update_id = '{update_id}';"
+            await db.execute(query)
+            await db.commit()
+
+    @staticmethod
+    async def track_update(update_id):
+        async with aiosqlite.connect('../db/animal_monitor.db') as db:
+            query = f"INSERT OR IGNORE INTO proceeded_messages (update_id) VALUES('{update_id}');"
+            await db.execute(query)
+            await db.commit()
+
+    @staticmethod
+    async def is_update_resolved(update_id):
+        async with aiosqlite.connect('../db/animal_monitor.db') as db:
+            query = f"SELECT is_resolved FROM proceeded_messages WHERE update_id = '{update_id}';"
+            query_result = await db.execute(query)
+            await db.commit()
+            is_resolved = await query_result.fetchall()
+            if len(is_resolved) == 0:
+                return False
+            if len(is_resolved) == 1 and is_resolved[0][0] == 'True':
+                return True
+            else:
+                return False
 
 
 if __name__ == '__main__':
